3cb235a5d379317370de7359ac3dd97e20d5515a
Nookler <artemselifanov@gmail.com>
2017-04-25 12:28:58 +0300

    Update my.js

--- my.js
+++ my.js
@@ -187,7 +187,7 @@ function displayToc(filter) {
    var tocHead2 = 'Дата';
    var tocTool2 = 'Click to sort by date';
    var tocHead3 = 'Labels22';
-   var tocTool3 = 'Артем2';
+   var tocTool3 = '2';
    if (sortBy == "titleasc") { 
       tocTool1 += ' (descending)';
       tocTool2 += ' (newest first)';
